0dd57b200e2826de18b399f00f6b8913bef6ec77
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-06 13:33:43 +0100

    update/ride-management

--- system.sol
+++ system.sol
@@ -93,4 +93,138 @@ contract ThemePark {
         token.increaseTotalSupply(_numTokens);
     }
 
+
+
+    // ========= THEME PARK RIDE MANAGEMENT =========
+
+    // Events
+    event EnjoyRide(string);
+    event NewRide(string, uint);
+    event RetireRide(string);
+
+    // Ride struct
+    struct Ride {
+        string name;
+        uint price;
+        bool state;
+    }
+
+    // Mapping to relate name of ride with ride
+    mapping(string => Ride) public rides;
+
+    // Save rides in array
+    string[] listOfRides;
+
+    // Mapping to relate client with history at the theme park
+    mapping(address => string[]) clientHistory; 
+
+    // Function to create new rides
+    function addRide(string memory _name, uint _price) public OnlyOwner(msg.sender){
+
+        // Create new ride 
+        rides[_name] = Ride(_name, _price, true);
+
+        // Save ride to list
+        listOfRides.push(_name);
+
+        // Emit event
+        emit NewRide(_name, _price);
+    }
+
+    // Function to retire rides
+    function retireRide(string memory _name) public OnlyOwner(msg.sender) returns(bool){
+
+        // Check the ride exists
+        bool exists = false;
+
+        for(uint i = 0; i < listOfRides.length; i++){
+            if (keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked(listOfRides[i]))) {
+                exists = true;
+                break;
+            } 
+        }
+
+        // Require ride exists
+        require(exists, 'Ride does not exist yet');
+
+        // Change the state of the ride
+        rides[_name].state = false;
+
+        // Emit event
+        emit RetireRide(_name);
+
+        // Return the value
+        return true;
+    }
+
+    // Function to see rides
+    function checkRides() public view returns(string[] memory) {
+        
+        // Check that there are rides
+        require(listOfRides.length > 0, 'No rides added yet');
+
+        // Return the array of rides
+        return listOfRides;
+    }
+
+    // Function to hop on a ride and pay in tokens
+    function useRide(string memory _name) public {
+
+        // Check the ride exists
+        bool exists = false;
+
+        for(uint i = 0; i < listOfRides.length; i++){
+            if (keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked(listOfRides[i]))) {
+                exists = true;
+                break;
+            } 
+        }
+
+        // Require ride exists
+        require(exists, 'Ride does not exist yet');
+
+        // Check price ride
+        uint price = rides[_name].price;
+
+        // Check status of ride
+        require(rides[_name].state == true, 'Ride is out of service');
+
+        // Check person has the tokens
+        require(price < tokensLeft(), 'Not enough tokens for this ride');
+
+        // Tranfer tokens to the theme park
+        token.transferThemePark(msg.sender, address(this), price);
+
+        // Save client history
+        clientHistory[msg.sender].push(_name);
+
+        // Emit event
+        emit EnjoyRide(_name);
+    }
+
+    // Check client history
+    function checkHistory() public view returns(string[] memory) {
+
+        // Return the history
+        return clientHistory[msg.sender];
+    }
+
+    // Function to allow client to return tokens when leaving the theme park
+    function returnTokens(uint _numTokens) public payable {
+
+        // Check number of tokens is greater than 0
+        require(_numTokens > 0, 'Cannot return tokens');
+
+        // User must posses the number of tokens to return
+        require(_numTokens <= tokensLeft(), 'You do not have the tokens you wish to return');
+
+        // Client returns tokens
+        token.transferThemePark(msg.sender, address(this), _numTokens);
+
+        // Theme park returns ether to client
+        uint returnValue = assignPrice(_numTokens);
+        (bool success, ) = payable(msg.sender).call{value: returnValue}('');
+        require(success, 'ETH transfer failed');
+    }
+
 }
